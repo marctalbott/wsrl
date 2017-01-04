2fb23df74a470a7a077e519303098cb6b4b6fc6d
Marcus B. Talbott <mbt1@tcl216-7.cs.williams.edu>
2017-01-04 14:10:03 -0500

    add displays for avatar and messages

--- asset/game.js
+++ asset/game.js
@@ -8,16 +8,32 @@ window.onload = function() {
   } else {
     // Initialize the game
     Game.init();
-    document.getElementById('wsrl-main-display').appendChild(Game.display.main.o.getContainer()); // puts display object in html
+
+    // add containers to html
+    document.getElementById('wsrl-avatar-display').appendChild(Game.getDisplay('avatar').getContainer());
+    document.getElementById('wsrl-main-display').appendChild(Game.getDisplay('main').getContainer());
+    document.getElementById('wsrl-message-display').appendChild(Game.getDisplay('message').getContainer());
+
   }
 };
 
 // Hash
 var Game = {
+
+  _DISPLAY_SPACING: 1.1,
   display: {
-    SPACING: 1.1,
     main: {
       w: 80,
+      h: 24,
+      o: null
+    },
+    message: {
+      w: 100,
+      h: 8,
+      o: null
+    },
+    avatar: {
+      w: 20,
       h: 24,
       o: null
     }
@@ -25,11 +41,16 @@ var Game = {
   // key init, value function <-- dope!
   init: function() {
     console.log("game init");
-    this.display.main.o = new ROT.Display(
-      { width: this.display.main.w,
-        height: this.display.main.h,
-        spacing: Game.display.SPACING}
-    );
+    for (var display_key in this.display) {
+      this.display[display_key].o = new ROT.Display(
+        { width: this.display[display_key].w,
+          height: this.display[display_key].h,
+          spacing: Game._DISPLAY_SPACING }
+      );
+    }
+
+    console.dir(this.display);
+
     this.renderMain();
   },
 
@@ -42,16 +63,26 @@ var Game = {
 
   renderMain: function() {
     var d = this.display.main.o;
-    d.setOptions({bg: "#0a0"});
     for (var i = 0; i < 24; i++) {
-      d.drawText(0,i,"%c{red}helloworld");
-      d.drawText(10,i,"%c{blue}%b{white}helloworld");
-      d.drawText(20,i,"%c{green}helloworld");
-      d.drawText(30,i,"%c{yellow}helloworld");
-      d.drawText(40,i,"%c{purple}helloworld");
-      d.drawText(50,i,"%c{cyan}helloworld");
-      d.drawText(60,i,"%c{orange}helloworld");
-      d.drawText(70,i,"%c{gray}helloworld");
+      d.drawText(0,i,"%c{red}mapdisplay");
+      d.drawText(10,i,"%c{blue}%b{white}mapdisplay");
+      d.drawText(20,i,"%c{green}mapdisplay");
+      d.drawText(30,i,"%c{yellow}mapdisplay");
+      d.drawText(40,i,"%c{purple}mapdisplay");
+      d.drawText(50,i,"%c{cyan}mapdisplay");
+      d.drawText(60,i,"%c{orange}mapdisplay");
+      d.drawText(70,i,"%c{gray}mapdisplay");
+    }
+    var a = this.display.avatar.o;
+    for (var i = 0; i < 24; i++) {
+      a.drawText(0,i,"%c{gray}youravatar");
+      a.drawText(10,i,"%c{pink}%b{white}youravatar");
+    }
+    var m = this.display.message.o;
+    for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 100; j+= 8) {
+        m.drawText(j,i, "messages");
+      }
     }
   }
 };
